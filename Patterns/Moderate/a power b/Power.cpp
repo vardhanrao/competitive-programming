77a17292273ea894d1649baea7ea80b3ec4d1437
devikasugathan <devikasugathan007@gmail.com>
2018-10-29 00:46:28 +0530

    Updated

---
+++ Patterns/Moderate/a power b/Power.cpp
@@ -0,0 +1,17 @@
+# include<bits/stdc++.h>
+using namespace std;
+int main() 
+{	 int k;
+	cin>>k;
+	while(k--){
+	    int n,s;
+	    long long int p;
+		cin>>n>>s;
+		p=pow(n,s);
+		long long int r=p%1000000007;
+		cout<<r<<endl;
+		
+		}
+ 
+ 
+}  
